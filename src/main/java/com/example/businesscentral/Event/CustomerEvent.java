0808ea9913ce3aa9fb0affd300420bafaf4e94bc
Fu_Ubuntu <fuyunfei119@gmail.com>
2023-05-08 19:08:28 +0200

    add event trigger

---
+++ src/main/java/com/example/businesscentral/Event/CustomerEvent.java
@@ -0,0 +1,23 @@
+package com.example.businesscentral.Event;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomerEvent extends ApplicationEvent {
+
+    public CustomerEvent(Object source) {
+        super(source);
+    }
+
+    public static class OnBeforeCheckIfCustomerHasOverDueBalance extends CustomerEvent {
+        private Boolean IsHandled;
+
+        public OnBeforeCheckIfCustomerHasOverDueBalance(Object source, Boolean isHandled) {
+            super(source);
+            this.IsHandled = isHandled;
+        }
+
+        public Boolean getHandled() {
+            return IsHandled;
+        }
+    }
+}
---
+++ src/main/java/com/example/businesscentral/EventListener/CustomerEventListener.java
@@ -0,0 +1,14 @@
+package com.example.businesscentral.EventListener;
+
+import com.example.businesscentral.Event.CustomerEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerEventListener {
+    @EventListener
+    public void OnBeforeCheckIfCustomerHasOverDueBalance(CustomerEvent.OnBeforeCheckIfCustomerHasOverDueBalance event) {
+        System.out.println(event.getSource());
+        System.out.println(event.getHandled());
+    }
+}
